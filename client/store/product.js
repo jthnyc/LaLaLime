34bfdc7f887349624195c359fae75ae4c7fcf0b3
Elizabeth Einstein <eeinstein134@gmail.com>
2020-01-13 16:30:54 -0500

    Feature: create new store for leggings and products and updated tests for leggings

--- client/store/product.js
+++ client/store/product.js
@@ -4,46 +4,30 @@ import axios from 'axios'
  * ACTION TYPES
  */
 const GOT_PRODUCTS = 'GET_PRODUCTS'
-const GOT_SINGLE_PRODUCT = 'GET_SINGLE_PRODUCT'
 
 /**
  * INITIAL STATE
  */
 const products = {
-  products: [],
-  singleProduct: {}
+  products: []
 }
 
 /**
  * ACTION CREATORS
  */
 const gotProducts = () => ({type: GOT_PRODUCTS})
-const gotSingleProduct = singleProduct => ({
-  type: GOT_SINGLE_PRODUCT,
-  singleProduct
-})
 
 /**
  * THUNK CREATORS
  */
 export const getProducts = () => async dispatch => {
   try {
-    const {data} = await axios.get('/api/products/all')
+    const {data} = await axios.get('/api/products')
     dispatch(gotProducts(data))
   } catch (error) {
     console.error(error)
   }
 }
-
-export const getSingleProduct = singleProduct => async dispatch => {
-  try {
-    const {data} = await axios.get(`/api/products/${singleProduct}`)
-    dispatch(gotSingleProduct(data))
-  } catch (error) {
-    console.error(error)
-  }
-}
-
 /**
  * REDUCER
  */
@@ -51,8 +35,6 @@ export default function(state = products, action) {
   switch (action.type) {
     case GOT_PRODUCTS:
       return {...state, products: action.products}
-    case GOT_SINGLE_PRODUCT:
-      return {...state, singleProduct: action.singleProduct}
     default:
       return state
   }
