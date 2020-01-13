c57207b3c79246072eadd49768ec53bd5b03dd4d
Elizabeth Einstein <eeinstein134@gmail.com>
2020-01-13 16:34:24 -0500

    Feature: added legging to combinereducer in store

--- client/store/legging.spec.js
+++ client/store/legging.spec.js
@@ -1,7 +1,7 @@
 /* global describe beforeEach afterEach it */
 
 import {expect} from 'chai'
-import {getLeggings, getSingleLegging} from './leggings'
+import {getLeggings, getSingleLegging} from './legging'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import configureMockStore from 'redux-mock-store'
