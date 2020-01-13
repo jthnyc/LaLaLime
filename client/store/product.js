aed3f57835111e4f12083b15a7784861310b4994
Elizabeth Einstein <eeinstein134@gmail.com>
2020-01-13 15:45:14 -0500

    Feature: redux actions, thunks and reducer for products and single products page

---
+++ client/store/product.js
@@ -0,0 +1,59 @@
+import axios from 'axios'
+
+/**
+ * ACTION TYPES
+ */
+const GOT_PRODUCTS = 'GET_PRODUCTS'
+const GOT_SINGLE_PRODUCT = 'GET_SINGLE_PRODUCT'
+
+/**
+ * INITIAL STATE
+ */
+const defaultProducts = {
+  products: [],
+  singleProduct: {}
+}
+
+/**
+ * ACTION CREATORS
+ */
+const gotProducts = () => ({type: GOT_PRODUCTS})
+const gotSingleProduct = singleProduct => ({
+  type: GOT_SINGLE_PRODUCT,
+  singleProduct
+})
+
+/**
+ * THUNK CREATORS
+ */
+export const getProducts = () => async dispatch => {
+  try {
+    const {data} = await axios.get('/api/products/all')
+    dispatch(gotProducts(data || defaultProducts.products))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const getSingleProduct = singleProduct => async dispatch => {
+  try {
+    const {data} = await axios.get(`/api/products/${singleProduct}`)
+    dispatch(gotSingleProduct(data || defaultProducts.singleProduct))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultProducts, action) {
+  switch (action.type) {
+    case GOT_PRODUCTS:
+      return {...state, products: action.products}
+    case GOT_SINGLE_PRODUCT:
+      return {...state, singleProduct: action.singleProduct}
+    default:
+      return state
+  }
+}
