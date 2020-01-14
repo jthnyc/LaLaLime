404b1bc6ce3e914579512ef2dcf6d104b8838495
Joanna Huang <joannathhuang@gmail.com>
2020-01-14 14:48:18 -0500

    Feature: item component in progress

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { CartItem } from './cart-item'
+import CartItem from './cart-item'
 
 class Cart extends React.Component {
   constructor() {
@@ -29,30 +29,13 @@ class Cart extends React.Component {
 
   render() {
     return (
-      <div id="cart-page">
+      <div className="cart-page">
         <h2>Shopping cart</h2>
-        <table>
-          <tbody>
-            {/* <CartItem /> */}
-            <tr>
-              <td>
-                <img
-                  src="https://cdn.shopify.com/s/files/1/2185/2813/products/W5824R_03093R_1_8f37f807-0efa-4472-b478-6c353a5b5670.jpg?v=1578621956"
-                  width="100"
-                  height="120"
-                />
-              </td>
-              <td>Super Fast Tron Leggings</td>
-              <td>$20</td>
-              <td>SKU: W5824R030930</td>
-              <td>
-                <button type="button">-</button>
-                <button type="button">1</button>
-                <button type="button">+</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="cart-list">
+          <CartItem />
+          <CartItem />
+          <CartItem />
+        </div>
       </div>
     )
   }
