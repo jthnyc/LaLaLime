7b7caaae0faeb992e750b1c6426b66e3f2fd0177
Elizabeth Einstein <eeinstein134@gmail.com>
2020-01-14 17:38:52 -0500

    Fixed: Rearranged links on navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -12,19 +12,19 @@ const Navbar = ({handleClick, isLoggedIn}) => (
         <div>
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
+          <Link to="/products">Products</Link>
+          <Link to="/cart">My Cart</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
-          <Link to="/cart">My Cart</Link>
-          <Link to="/products">Products</Link>
         </div>
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
+          <Link to="/products">Products</Link>
+          <Link to="/cart">My Cart</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
-          <Link to="/cart">My Cart</Link>
-          <Link to="/products">Products</Link>
         </div>
       )}
     </nav>
