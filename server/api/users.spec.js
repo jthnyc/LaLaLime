f93192438ddc7e7c38e83af1c028a88bfce55a21
Rachel Taverna <rtaverna96@gmail.com>
2020-01-14 16:53:29 -0500

    Added tests post user

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -12,21 +12,58 @@ describe('User routes', () => {
   })
 
   describe('/api/users/', () => {
-    const codysEmail = 'cody@puppybook.com'
+    const rachelsEmail = 'rachel@gmail.com'
 
     beforeEach(() => {
       return User.create({
-        email: codysEmail
+        email: rachelsEmail
       })
     })
 
     it('GET /api/users', async () => {
-      const res = await request(app)
-        .get('/api/users')
-        .expect(200)
+      const res = await request(app).get('/api/users')
 
+      expect(res.status).to.be.equal(200)
       expect(res.body).to.be.an('array')
-      expect(res.body[0].email).to.be.equal(codysEmail)
+      expect(res.body[0].email).to.be.equal(rachelsEmail)
     })
-  }) // end describe('/api/users')
-}) // end describe('User routes')
+
+    describe('GET /api/users/:userId', async () => {
+      it('gets the user with the specified id', async () => {
+        const res = await request(app)
+          .get('/api/users/1')
+          .expect(200)
+
+        expect(res.body).to.be.an('array')
+        expect(res.body.length).to.be.equal(1)
+        expect(res.body[0].email).to.be.equal(rachelsEmail)
+      })
+
+      describe('POST /api/users', () => {
+        it('creates a new user and responds with the user', async () => {
+          const res = await request(app)
+            .post('/api/users')
+            .send({
+              email: 'rachel@gmail.com'
+            })
+
+          expect(res.status).to.be.equal(201)
+          expect(res.body).to.be.an('object')
+          expect(res.body.email).to.equal('rachel@gmail.com')
+        })
+
+        it('saves the new user to the database', async () => {
+          const newUser = await User.findOne({
+            where: {
+              email: 'rachel@gmail.com'
+            }
+          })
+
+          expect(newUser).to.be.an('object')
+          expect(newUser.email).to.equal('rachel@gmail.com')
+        })
+        it('responds with an error if there is already a user with that email', async () => {})
+      })
+    })
+  })
+})
