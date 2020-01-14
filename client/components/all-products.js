d2bac6aac6f0cec0a1871c3bcb8350e4e9589046
Elizabeth Einstein <eeinstein134@gmail.com>
2020-01-14 14:30:27 -0500

    Feature: filled in products component

--- client/components/all-products.js
+++ client/components/all-products.js
@@ -1,8 +1,34 @@
 import React from 'react'
 import {getProducts} from '../store'
 import {connect} from 'react-redux'
+import Product from './product'
 
-class AllProducts extends React.Component {}
+/**
+ * COMPONENT
+ */
+
+class AllProducts extends React.Component {
+  componentDidMount() {
+    this.props.getProducts()
+  }
+
+  render() {
+    const products = this.props.products
+    return (
+      <div>
+        <div className="faceted-grid">
+          <div className="product-card-grid-all">
+            <div className="product-card">
+              {products.map(product => {
+                return <Product key={product.id} product={product} />
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 /**
  * CONTAINER
