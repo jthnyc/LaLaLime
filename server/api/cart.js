46deabee9cb25cd7f9f140f155626cd1fcdaf7d0
Yuanhao Zhang <cercle@bu.edu>
2020-01-15 18:51:45 -0500

    temporary comment out model Order validation allowNull

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,3 +1,23 @@
 const router = require('express').Router()
 const {Product, Order, User} = require('../db/models')
 module.exports = router
+
+router.post('/order', async (req, res, next) => {
+  try {
+    console.log('reqbody', req.body)
+    const newOrder = await Order.create()
+    console.log('newOrder', newOrder)
+    // const currentUser = User.findOne({
+    //   where: {
+    //     id: req.body.userId
+    //   }
+    // })
+    // newOrder.setUser(currentUser)
+    // const currentProduct = await Product.findOne({
+    //   where: {id: req.body.productId}
+    // })
+    // newOrder.hasProduct(currentProduct)
+  } catch (error) {
+    next(error)
+  }
+})
