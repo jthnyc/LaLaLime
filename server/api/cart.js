bbe000988be487159004405727f44d84dbb05238
Joanna Huang <joannathhuang@gmail.com>
2020-01-16 13:00:33 -0500

    Feature: backend cartview

--- server/api/cart.js
+++ server/api/cart.js
@@ -2,6 +2,29 @@ const router = require('express').Router()
 const {Product, Order, User, ProductOrder} = require('../db/models')
 module.exports = router
 
+router.get('/:userId', async (req, res, next) => {
+  try {
+    console.log('REQ.PARAMS: ', req.params)
+    const cart = await Order.findOne({
+      where: {
+        userId: req.params.userId,
+        status: 'pending'
+      }
+    })
+    console.log('CART: ', cart)
+    const orderId = cart.id
+    const productList = await ProductOrder.findAll({
+      where: {
+        orderId: orderId
+      },
+      include: [{model: Product, as: 'product'}]
+    })
+    res.json(productList)
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.post('/order', async (req, res, next) => {
   try {
     const existingOrder = await Order.findOne({
@@ -22,7 +45,7 @@ router.post('/order', async (req, res, next) => {
       where: {orderId: currentOrder.id, productId: req.body.productId}
     })
 
-    //check to see if the current order has this kind of product befor
+    //check to see if the current order has this kind of product before
     if (!currentProductOrder) {
       //adding current product to the current order
       const currentProduct = await Product.findOne({
