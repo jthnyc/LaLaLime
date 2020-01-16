690448cbef580a356675eb43c3463a9b385c8624
Yuanhao Zhang <cercle@bu.edu>
2020-01-15 21:44:26 -0500

    Feature/Fix: at the single product page able to add product to current pending order instead of creating new order each time

--- server/api/cart.js
+++ server/api/cart.js
@@ -4,11 +4,24 @@ module.exports = router
 
 router.post('/order', async (req, res, next) => {
   try {
-    const newOrder = await Order.create({userId: req.body.userId})
+    const existingOrder = await Order.findOne({
+      where: {
+        userId: req.body.userId,
+        status: 'pending'
+      }
+    })
+    let currentOrder
+    //if the user does not have pending order, create a new order
+    if (!existingOrder) {
+      currentOrder = await Order.create({userId: req.body.userId})
+    } else {
+      currentOrder = existingOrder
+    }
+    //adding current product to the current order
     const currentProduct = await Product.findOne({
       where: {id: req.body.productId}
     })
-    await newOrder.addProduct(currentProduct)
+    await currentOrder.addProduct(currentProduct)
     res.sendStatus(201)
   } catch (error) {
     next(error)
