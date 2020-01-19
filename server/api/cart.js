083fd88bdef9df722ad94c5699c85965cafa2e4d
Rachel Taverna <rtaverna96@gmail.com>
2020-01-19 10:52:41 -0500

    all functional except initial empty cart for guest user with cercles requested change

--- server/api/cart.js
+++ server/api/cart.js
@@ -160,6 +160,7 @@ router.delete('/:userId', async (req, res, next) => {
       }
     })
     await currentProductOrder.destroy()
+    res.sendStatus(202)
   } catch (error) {
     next(error)
   }
