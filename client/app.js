a4114742daa4cc5baf058bcb22a9be4954c86f8c
Elizabeth Einstein <eeinstein134@gmail.com>
2020-01-21 18:01:25 -0500

    Fixed: quotations

--- client/app.js
+++ client/app.js
@@ -8,10 +8,7 @@ const App = () => {
     <div>
       <Navbar />
 
-      <StripeProvider
-        apiKey="pk_test_Ps27Rz88orWwoLpKW4ZyiuL600L8nGpOL0
-"
-      >
+      <StripeProvider apiKey="pk_test_Ps27Rz88orWwoLpKW4ZyiuL600L8nGpOL0">
         <Elements>
           <Routes />
         </Elements>
