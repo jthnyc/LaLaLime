546b5d32e8bc864d071d781e85bce6a5770715e1
Joanna Huang <joannathhuang@gmail.com>
2020-01-22 18:35:15 -0500

    Fixed home component

--- client/components/home.js
+++ client/components/home.js
@@ -5,37 +5,27 @@ import {Link} from 'react-router-dom'
  * COMPONENT
  */
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className="home-container">
+const Home = () => {
+  return (
+    <div>
+      <div className="home-container">
+        <div>
+          <h2 className="home-header">New Arrivals</h2>
           <div>
-            <h2 className="home-header">New Arrivals</h2>
-            <div>
-              <Link to="/products">
-                <button
-                  className="home-button"
-                  type="button"
-                  onClick={this.handleclick}
-                >
-                  Shop Now
-                </button>
-              </Link>
-            </div>
+            <Link to="/products">
+              <button className="home-button" type="button">
+                Shop Now
+              </button>
+            </Link>
           </div>
-          <img
-            className="home-image"
-            src="https://classpass-res.cloudinary.com/image/upload/f_auto,q_auto/n64dslxthjbzh2y53tgz.png"
-          />
         </div>
+        <img
+          className="home-image"
+          src="https://classpass-res.cloudinary.com/image/upload/f_auto,q_auto/n64dslxthjbzh2y53tgz.png"
+        />
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-/**
- * CONTAINER
- */
 
 export default Home
