92bd1c43a48e7a8c6503a82a539d2b7947fc8ac5
Rachel Taverna <rtaverna96@gmail.com>
2020-01-22 12:06:25 -0500

    Feature: guests home page redirects to all products

--- client/components/order-item.js
+++ client/components/order-item.js
@@ -2,19 +2,18 @@ import React from 'react'
 
 const OrderItem = props => {
   const {item} = props
-  console.log('order-item props', props)
   return (
     <div key={item.id} className="cart-item">
       <div className="cart-item-row">
-        <img src={item.imageUrl} width="100" height="120" />
+        <img src={item.product.imageUrl} width="100" height="120" />
         <div className="cart-item-detail">
           <a href={`/products/${item.id}`}>
-            <b>{item.name}</b>
+            <b>{item.product.name}</b>
           </a>
-          <div>SKU: {item.SKU}</div>
-          <div>Color: {item.color}</div>
-          <div>Size: {item.size}</div>
-          <b>${item.price}</b>
+          <div>SKU: {item.product.SKU}</div>
+          <div>Color: {item.product.color}</div>
+          <div>Size: {item.product.size}</div>
+          <b>${item.product.price}</b>
         </div>
         <div className="quantity-wrapper">
           <div>quantity: {item.quantity}</div>
