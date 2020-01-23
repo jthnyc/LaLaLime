48fe2f217954aa7f05ea55cce8ca594182d47f0a
Elizabeth Einstein <eeinstein134@gmail.com>
2020-01-22 23:26:40 -0500

    Fixed: spacing between first and last name in order history

--- client/components/completed-order.js
+++ client/components/completed-order.js
@@ -7,7 +7,9 @@ const CompletedOrder = props => {
     <div className="completed-order">
       <div>Order number: {order.id}</div>
       <div>Placed on: {order.updatedAt.slice(0, 10)}</div>
-      <div>Recipient: {order.firstName + order.lastName}</div>
+      <div>
+        Recipient: {order.firstName} {order.lastName}
+      </div>
       <div>Address: {order.address}</div>
     </div>
   )
